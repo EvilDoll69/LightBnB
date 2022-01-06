b8dbfe13edf1bae0ee9023ce31f64f6502e99adf
EvilDoll69 <tetiana.fediushyn@gmail.com>
2022-01-06 21:20:48 +0000

    Reservation database was binded

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -73,7 +73,13 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit];
+  return pool
+  .query(`SELECT * FROM reservations WHERE guest_id = $1 LIMIT $2`, values) //amout of binging supposed to match
+  .then((result) => result.rows)
+  .catch((err) => {
+    console.log(err.message);
+  })
 }
 exports.getAllReservations = getAllReservations;
 
